8af88ec16aac1494056749c9a9a1742cbdee172b
Konstantin <konstantinkolovski_zh21@schoolmath.eu>
2023-03-09 18:07:28 +0200

    problem5

---
+++ 05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            string censorLength;
+            for (int s1 = 0; s1 < input.Length; s1++)  
+            {
+                censorLength = new string('*', word.Length); 
+                input[s1] = input[s1].Replace(word, censorLength);
+            }
+            Console.WriteLine(string.Join(" ", input));           
+        }
+    }
+}
